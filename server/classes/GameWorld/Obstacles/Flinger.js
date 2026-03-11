8775aed690b799c0c67476b6a44df25c70699804
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-03-10 21:23:32 -0400

    Made some refactors for the actual gameplay aspect of this

---
+++ server/classes/GameWorld/Obstacles/Flinger.js
@@ -0,0 +1,17 @@
+export class Flinger {
+    constructor(world) {
+        this.world = world;
+    }
+
+    /**
+     * These placements will depend on the map
+     * Some maps might not even have these
+     */
+    getFlingerPlacements() {
+
+    }
+
+    initFlingerPhysics() {
+
+    }
+}
