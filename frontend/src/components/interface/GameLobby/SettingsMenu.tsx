e6c86a0e1e9c4a404f150f1b50eeef11440a9e6f
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-03-22 18:07:49 -0400

    Added some sound sliders for the settings menu

--- frontend/src/components/interface/GameLobby/SettingsMenu.tsx
+++ frontend/src/components/interface/GameLobby/SettingsMenu.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useRef } from 'react';
+import { useLayoutEffect, useRef, useState } from 'react';
 import '../../../styles/settings_menu.css';
 import { scroll_reveal } from '../../../utils/consts/ScrollReveal';
 
@@ -6,11 +6,24 @@ interface SettingsMenuProps {
     toggleSettings: (e: React.MouseEvent) => void; 
 }
 
+interface SoundSettingsType {
+    sfx: number,
+    music: number,
+    other: number
+}
+
+export const DEFAULT_SOUND_VALUES = {
+    sfx: 50,
+    music: 50,
+    other: 50
+}
+
 const SettingsMenu = ({ toggleSettings } : SettingsMenuProps) => {
     // this will prob utilize a global state
     // so these settings can be saved throughout the game
     // but these are basic visuals for now
     const menuRef = useRef<HTMLDivElement>(null);
+    const [ soundValues, setSoundValues ] = useState<SoundSettingsType>(DEFAULT_SOUND_VALUES);
 
     useLayoutEffect(() => {
         if(menuRef.current) {
@@ -22,19 +35,68 @@ const SettingsMenu = ({ toggleSettings } : SettingsMenuProps) => {
         }
     }, []);
 
+    const handleSoundValueChange = (e: React.ChangeEvent<HTMLInputElement>, soundType: string) => {
+        const value = parseInt(e.target.value);
+        setSoundValues(prev => ({ ...prev, [soundType]: value }));
+    }
+
     return (
         <div ref={menuRef} className='settings_menu'>
             <h1 className='header1'>Game Settings</h1>
 
             {/* This will prob be a 2x2 grid container */}
             <div className='controls_container'>    
+                {/*       */}
                 {/* Sound */}
+                {/*       */}
+                <div className="sound_settings_container">
+                    <div className='sound_input'>
+                        <label htmlFor='range'>Value: {soundValues.music}</label>
+                        <input type='range' 
+                            min={0} 
+                            max={100} 
+                            step={1} 
+                            value={soundValues.music}
+                            onChange={(e) => handleSoundValueChange(e, "music")}
+                        ></input>       
+                    </div>
 
+                    <div className='sound_input'>
+                        <label htmlFor='range'>Value: {soundValues.sfx}</label>
+                        <input type='range' 
+                            min={0} 
+                            max={100} 
+                            step={1} 
+                            value={soundValues.sfx}
+                            onChange={(e) => handleSoundValueChange(e, "sfx")}
+                        ></input>       
+                    </div>
 
+                    <div className='sound_input'>
+                        <label htmlFor='range'>Value: {soundValues.other}</label>
+                        <input type='range' 
+                            min={0} 
+                            max={100} 
+                            step={1} 
+                            value={soundValues.other}
+                            onChange={(e) => handleSoundValueChange(e, "other")}
+                        ></input>       
+                    </div>
+                </div>
+                
+                {/*                   */}
                 {/* Keyboard Controls */}
+                {/*                   */}
+                <div className="keyboard_settings_container">
 
+                </div>
 
+                {/*       */}
                 {/* Login */}
+                {/*       */}
+                <div className="auth_settings_container">
+
+                </div>
             </div>
 
             <button onClick={toggleSettings} className='exit_settings_button'>X</button>
