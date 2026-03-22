058cdd354acfc45a2a197e4edaad321744c7985e
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-03-22 17:46:48 -0400

    Made rgba generator for the player list

--- frontend/src/components/interface/GameLobby/PlayerList.tsx
+++ frontend/src/components/interface/GameLobby/PlayerList.tsx
@@ -13,13 +13,21 @@ const PlayerList = () => {
         });
     }, []);
 
+    const generateRandomRGBAColor = () => {
+        const red = `${Math.floor(Math.random() * 255)}`;
+        const blue = `${Math.floor(Math.random() * 255)}`;
+        const green = `${Math.floor(Math.random() * 255)}`;
+
+        return `rgba(${red}, ${blue}, ${green}, 0.7)`;
+    }
+
     return (
         <div className="player_list_container">
             <div className="player_list_header">Total Players: {total_players}</div>
 
             <div className="players_container">
                 {pending_player_ids?.map((player_id, index) => (
-                    <div key={index + player_id} className="player_list_item_container">
+                    <div style={{ backgroundColor: generateRandomRGBAColor()}} key={index + player_id} className="player_list_item_container">
                         <div className="player_list_item">Player {player_id.slice(0, 10)}</div>
                         <div className="player_icon"></div>
                     </div>
