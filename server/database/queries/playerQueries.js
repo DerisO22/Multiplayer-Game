207e432a13614beabc3c583b8a8ab2282384e0a6
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-03-28 23:15:49 -0400

    Added query to get all player info

--- server/database/queries/playerQueries.js
+++ server/database/queries/playerQueries.js
@@ -1,4 +1,9 @@
 export const playerQueries = {
     GET_ALL_PLAYER_INFORMATION: 
-        `SELECT * FROM`
+        `SELECT *
+        FROM players p
+        JOIN player_keybinds pk ON p.clerk_user_id = pk.clerk_user_id
+        JOIN player_stats pst ON p.clerk_user_id = pst.clerk_user_id
+        JOIN player_sounds pso ON p.clerk_user_id = pso.clerk_user_id
+        WHERE p.clerk_user_id = $1;`,
 }
