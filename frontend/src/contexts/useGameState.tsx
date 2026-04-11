e2bcb1c1e12f0397a38d9994b10bf883cf277fec
deris.o2024 <deris.omalley@mymail.champlain.edu>
2026-04-10 23:11:03 -0400

    Removed console logs

--- frontend/src/contexts/useGameState.tsx
+++ frontend/src/contexts/useGameState.tsx
@@ -27,7 +27,6 @@ export const GameProvider = ({ children }: GameProviderProps) => {
         if(!socket) return;
 
         const handleState = (state: GameState) => {
-            console.log(state.teamInfo)
             setGameState(state);
         }
 
